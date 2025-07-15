c8c4f5a09d1830b3dee5da3e4139fa04549c65d2
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-07-15 11:51:50 +0300

    feature: add rest of the exercises solutions from chapter 11

--- chapter_11/exercises.ex
+++ chapter_11/exercises.ex
@@ -54,7 +54,7 @@ defmodule StringAndBinaries do
   defp apply_operator("*", a, b), do: a * b
   defp apply_operator("/", a, b), do: a / b
 
-  # Exercise 4
+  # Exercise 5
   def center(word_list) do
     longest_word_len =
       word_list
@@ -66,7 +66,6 @@ defmodule StringAndBinaries do
       word_len = String.length(word)
       total_padding = longest_word_len - word_len
       left_pad = div(total_padding, 2)
-      right_pad = total_padding - left_pad
 
       centered =
         word
@@ -75,5 +74,35 @@ defmodule StringAndBinaries do
 
       IO.puts(centered)
     end)
+  end
+
+  # Exercise 6
+  # "oh. a DOG. woof. "
+  # OH. A dog. Woof. "
+  def capitalize_sentence(sentence) do
+    sentence |> String.split(". ") |> Enum.map(&String.capitalize(&1)) |> Enum.join(". ")
+  end
+
+  # Exercise 7 - rewrite the function from chapter_7 and read from file.
+  def apply_tax(file_name, tax_rates) do
+    with {:ok, file} <- File.open(file_name) do
+      IO.stream(file, :line)
+      |> Enum.take_while(&(&1 != "\n"))
+      |> Enum.drop(1)
+      |> Enum.map(&String.replace(&1, "\n", ""))
+      |> Enum.map(&parse_order(&1, tax_rates))
+    else
+      {:error, _} -> :error
+    end
+  end
+
+  defp parse_order(line, tax_rates) do
+    [id, ":" <> ship_to_str, net_str] = String.split(line, ",")
+
+    ship_to = String.to_atom(ship_to_str)
+    net = String.to_float(net_str)
+    total = net + net * Keyword.get(tax_rates, ship_to, 0.0)
+
+    %{id: id, ship_to: ship_to, net: net, total: total}
   end
 end
